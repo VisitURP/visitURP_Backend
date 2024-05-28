2b7bb32b7755e3afcc959fdfc15f0d135e38e0f1
aleolayar0104 <alessandra.reano@urp.edu.pe>
2024-05-28 09:30:01 -0400

    first files

---
+++ app/Http/Controllers/UserURPController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\user_URP;
+use Illuminate\Http\Request;
+
+class UserURPController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(user_URP $user_URP)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(user_URP $user_URP)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, user_URP $user_URP)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(user_URP $user_URP)
+    {
+        //
+    }
+}
